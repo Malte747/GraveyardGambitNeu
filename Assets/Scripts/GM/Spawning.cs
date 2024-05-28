ed204dac962db5e8e00fe909be982775b83be35c
Malteleh <148697349+Malteleh@users.noreply.github.com>
2024-05-29 00:37:30 +0200

    Random ausgang Block + Gegenlicht + general fixes

--- Assets/Scripts/GM/Spawning.cs
+++ Assets/Scripts/GM/Spawning.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Spawning : MonoBehaviour
 {
+    SzeneManager szeneManager;
     public GameObject playerPrefab;
     public Transform[] spawnPoints;
+    [SerializeField] private GameObject[] ExitToActivate;
+    [SerializeField] private GameObject gateDeactivate;
+     [SerializeField] private GameObject treeDeactivate;
+
 
     void Awake()
     {
@@ -24,5 +29,57 @@ public class Spawning : MonoBehaviour
         int randomIndex = Random.Range(0, spawnPoints.Length);
         Transform spawnPoint = spawnPoints[randomIndex];
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    void Start()
+    {
+        szeneManager = GameObject.Find("PlayerData").GetComponent<SzeneManager>();
+
+        if (szeneManager.raid >= 2 && szeneManager.raid <= 5)
+        {
+        foreach (GameObject obj in ExitToActivate)
+        {
+            obj.SetActive(false);
+        }
+
+        // Randomly select one of the objects and activate it
+        int randomIndex = Random.Range(0, ExitToActivate.Length);
+        ExitToActivate[randomIndex].SetActive(true);
+        if (randomIndex == 0)
+        {
+            gateDeactivate.SetActive(false);
+        }
+        else if (randomIndex == 2)
+        {
+            treeDeactivate.SetActive(false);
+        }
+        }
+
+        else if (szeneManager.raid >= 6 )
+        {
+        foreach (GameObject obj in ExitToActivate)
+        {
+            obj.SetActive(false);
+        }
+
+        int index1 = Random.Range(0, ExitToActivate.Length);
+        int index2;
+        do
+        {
+            index2 = Random.Range(0, ExitToActivate.Length);
+        } while (index1 == index2);
+
+        // Activate the two selected objects
+        ExitToActivate[index1].SetActive(true);
+        ExitToActivate[index2].SetActive(true);
+        if (index1 == 0 || index2 == 0)
+        {
+            gateDeactivate.SetActive(false);
+        }
+        if (index1 == 2 || index2 == 2)
+        {
+            treeDeactivate.SetActive(false);
+        }
+        }
     }
 }
